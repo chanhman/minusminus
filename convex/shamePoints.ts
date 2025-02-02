fb6520857c05082ad60fbda617a99627396417e5
chanhman <hello@chanh.design>
2025-02-01 20:54:02 -0700

    Add/delete person, graffiti

--- convex/shamePoints.ts
+++ convex/shamePoints.ts
@@ -1,4 +1,6 @@
 import { query } from "./_generated/server";
+import { mutation } from "./_generated/server";
+import { v } from "convex/values";
 
 export const get = query({
   args: {},
@@ -6,3 +8,23 @@ export const get = query({
     return await ctx.db.query("shamePoints").collect();
   },
 });
+
+export const addPersonToShame = mutation({
+  args: { name: v.string() },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("shamePoints", { name: args.name, shameCount: 0.0 });
+  },
+});
+
+export const deletePerson = mutation({
+  args: { id: v.id("shamePoints") },
+  handler: async (ctx, args) => {
+    await ctx.db.delete(args.id);
+  },
+});
+
+// export const updateShamePoints = mutation({
+//   args: { id: v.id("shamePoints") },
+//   handler: async (ctx, args) => {
+//   },
+// });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import { useQuery } from "convex/react";
+import { useState } from "react";
+import { useReward } from "react-rewards";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const addPersonToShame = useMutation(api.shamePoints.addPersonToShame);
+  const deletePerson = useMutation(api.shamePoints.deletePerson);
   const shamePoints = useQuery(api.shamePoints.get);
-  console.log(shamePoints);
+
+  const { reward: sorryReward } = useReward("rewardSorryId", "emoji", {
+    emoji: ["🫣"],
+  });
+  const { reward: shameReward } = useReward("rewardShameId", "emoji", {
+    emoji: ["🤡"],
+  });
+
   return (
     <>
       <h1>Minus minus</h1>
       <h2>Add person to shame</h2>
-      <form>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          await addPersonToShame({ name });
+          setName("");
+        }}
+      >
         <label htmlFor="personToShame">Name</label>
-        <input type="text" id="personToShame" name="personToShame" />
+        <input
+          type="text"
+          id="personToShame"
+          name="personToShame"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <button type="submit">Add person</button>
       </form>
       <h2>People to shame</h2>
       <ul>
-        {shamePoints?.map(({ _id, name }) => (
+        {shamePoints?.map(({ _id, name, shameCount }) => (
           <li key={_id}>
-            <h3>{name}</h3>
-            <button>🫣</button>
-            <button>😳</button>
+            <h3>
+              {name} ({shameCount})
+              <button
+                onClick={async () => {
+                  if (window.confirm("You sure??")) {
+                    deletePerson({ id: _id });
+                  }
+                }}
+              >
+                💩
+              </button>
+            </h3>
+            <button onClick={sorryReward}>
+              <span id="rewardSorryId" />
+              🫣 My bad
+            </button>
+            <button onClick={shameReward}>
+              <span id="rewardShameId" />
+              🤡 Shame
+            </button>
           </li>
         ))}
       </ul>
